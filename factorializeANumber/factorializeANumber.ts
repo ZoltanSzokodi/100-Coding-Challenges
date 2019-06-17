83956050a554ea8ffbe439fd2fb936eae7711f38
Rupert <zoltan.szokodi@hotmail.com>
2019-06-17 15:58:38 +0200

    Solutions up to 56

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,27 @@
+// 1st SOLUTION
 function factorializeANumber(num: number): number {
+  const myArr = [];
 
+  for (let i = 1; i <= num; i++) {
+    myArr.push(i);
+  }
+
+  return myArr.reduce((a, b) => a * b);
+}
+
+// 2nd SOLUTION
+function factorializeANumber2(num: number): number {
+  let total = 1;
+
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+
+  return total;
 }
 
 console.log(factorializeANumber(5));
 console.log(factorializeANumber(10));
+
+console.log(factorializeANumber2(5));
+console.log(factorializeANumber2(10));
--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,37 @@
+// 1st SOLUTION
 function fancyRide(l: number, fares: number[]): string {
+  const maxCost = 20;
+  const uberCars = {
+    0: "UberX",
+    1: "UberXL",
+    2: "UberPlus",
+    3: "UberBlack",
+    4: "UberSUV"
+  };
 
+  const prices = fares.map(price => price * l);
+  const affordableRides = prices.filter(val => val < maxCost);
+  const myRide = Math.max(...affordableRides);
+
+  return uberCars[prices.indexOf(myRide)];
+}
+
+// 2nd SOLUTION
+function fancyRide2(l: number, fares: number[]): string {
+  const uberCars = {
+    0: "UberX",
+    1: "UberXL",
+    2: "UberPlus",
+    3: "UberBlack",
+    4: "UberSUV"
+  };
+
+  for (let i = fares.length - 1; i >= 0; i--) {
+    if (fares[i] * l <= 20) {
+      return uberCars[i];
+    }
+  }
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide2(30, [0.3, 0.5, 0.7, 1, 1.3]));
